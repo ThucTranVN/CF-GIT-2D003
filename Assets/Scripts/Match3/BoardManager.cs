560c9037725b71da86483c849bf54afb50b04b4e
Thuc Tran <thuc.ttk@gmail.com>
2025-09-08 22:06:34 +0700

    [add] game pieces

--- Assets/Scripts/Match3/BoardManager.cs
+++ Assets/Scripts/Match3/BoardManager.cs
@@ -7,14 +7,17 @@ public class BoardManager : BaseManager<BoardManager>
     public GameObject TilePrefab; //For background
     public int BorderSize;
     public Tile[,] AllTiles;
-
+    public GamePiece[,] AllGamePieces;
+    public GameObject[] GamePiecePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         AllTiles = new Tile[Width, Height];
+        AllGamePieces = new GamePiece[Width, Height];
         SetupTiles();
         SetupCamera();
+        FillRandom();
     }
 
     void SetupTiles()
@@ -44,5 +47,47 @@ public class BoardManager : BaseManager<BoardManager>
         float verticleSize = (float)Height / 2f + (float)BorderSize;
         float horizontalSize = ((float)Width / 2f + (float)BorderSize) / aspectRatio;
         Camera.main.orthographicSize = (verticleSize > horizontalSize) ? verticleSize : horizontalSize;
+    }
+
+    private GameObject GetRandomGamePiece()
+    {
+        int randomIdx = Random.Range(0, GamePiecePrefab.Length);
+
+        if(GamePiecePrefab[randomIdx] == null)
+        {
+            Debug.LogWarning($"GamePiecePrefab at index = {randomIdx} is null");
+        }
+
+        return GamePiecePrefab[randomIdx];
+    }
+
+    private void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        if(gamePiece == null)
+        {
+            Debug.LogWarning("Invalid gamePiece component");
+            return;
+        }
+
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+        gamePiece.SetCoord(x, y);
+    }
+
+    private void FillRandom()
+    {
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                GameObject randomPiece = Instantiate(GetRandomGamePiece(),
+                    Vector3.zero, Quaternion.identity);
+
+                if(randomPiece != null)
+                {
+                    PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Match3/GamePiece.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    private bool isMoving = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move((int)transform.position.x - 1, (int)transform.position.y, 0.5f);
+        }
+    }
+
+    public void SetCoord(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
+
+    public void Move(int destinationX, int destinationY, float timeToMove)
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(MoveRoutine(new Vector3(destinationX, destinationY, 0), timeToMove));
+        }
+    }
+
+    private IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+
+        bool reachedDestionation = false;
+
+        float elapsedTime = 0f;
+
+        isMoving = true;
+
+        while (!reachedDestionation)
+        {
+            if(Vector3.Distance(transform.position, destination) < Mathf.Epsilon)
+            {
+                reachedDestionation = true;
+                transform.position = destination;
+                SetCoord((int)destination.x, (int)destination.y);
+            }
+
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f);
+
+            //t = Mathf.Sin(t * Mathf.PI * 0.5f);
+
+            //t = 1 - Mathf.Cos(t * Mathf.PI * 0.5f);
+
+            //t = t * t * (3 - 2 * t); //SmoothStep
+
+            t = t * t * t * (t * (t * 6 - 15f) + 10f); //Smoother Step
+
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+}
